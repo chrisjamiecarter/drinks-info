8f2d9abd13ffdf0b553223c6cfba475f280bd30c
chrisjamiecarter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-07-29 21:43:32 +0100

    Added response to model Mapping tests
    #16

---
+++ tests/DrinksInfo.Application.Tests/Mappings/V1/CategoryMappingTests.cs
@@ -0,0 +1,52 @@
+﻿using DrinksInfo.Contracts.Responses.V1;
+using DrinksInfo.Application.Mappings.V1;
+using Shouldly;
+using DrinksInfo.Application.Models;
+
+namespace DrinksInfo.Application.Tests.Mappings.V1;
+
+public class CategoryMappingTests
+{
+    [Fact]
+    public void ToCategory_ShouldReturnCategory_WhenResponseIsValid()
+    {
+        // Arrange.
+        string categoryName = "Cocktail";
+
+        var expected = new Category
+        {
+            Name = categoryName,
+        };
+
+        var response = new CategoryResponse
+        {
+            Name = categoryName,
+        };
+
+        // Act.
+        var category = response.ToCategory();
+        
+        // Assert.s
+        category.ShouldNotBeNull();
+        category.ShouldBeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void ToCategory_ShouldReturnCategory_WhenResponseNameIsNull()
+    {
+        // Arrange.
+        var expected = new Category
+        {
+            Name = string.Empty,
+        };
+
+        var response = new CategoryResponse();
+
+        // Act.
+        var category = response.ToCategory();
+
+        // Assert.
+        category.ShouldNotBeNull();
+        category.ShouldBeEquivalentTo(expected);
+    }
+}
---
+++ tests/DrinksInfo.Application.Tests/Mappings/V1/DrinkMappingTests.cs
@@ -0,0 +1,41 @@
+﻿using DrinksInfo.Contracts.Responses.V1;
+using DrinksInfo.Application.Mappings.V1;
+using Shouldly;
+using DrinksInfo.Application.Models;
+
+namespace DrinksInfo.Application.Tests.Mappings.V1;
+
+public class DrinkMappingTests
+{
+    [Fact]
+    public void ToDrink_ShouldReturnDrink_WhenResponseIsValid()
+    {
+        // Arrange.
+        string id = "12345";
+        string name = "test";
+        string category = "Cocktails";
+
+        var expected = new Drink
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            Instructions = string.Empty,
+        };
+
+        var response = new DrinkResponse
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            Instructions = null,
+        };
+
+        // Act.
+        var drink = response.ToDrink();
+
+        // Assert.s
+        drink.ShouldNotBeNull();
+        drink.ShouldBeEquivalentTo(expected);
+    }
+}
